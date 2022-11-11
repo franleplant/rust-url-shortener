a4a4e50ead50f3949afa23bf7d1b6ab25e7752ee
Fran Guijarro <franleplant@gmail.com>
2022-11-11 14:06:32 -0300

    basic connection to the db

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rust-url-shortener
 A fun learning exercise with Rust
+
+
+## Run db
+
+```bash
+
+docker run --name url_shortener -e POSTGRES_PASSWORD=postgres -p 54321:5432  -d postgres:13.7
+
+// try out the connection (requires local postgres installed)
+psql -p 54321 postgres://postgres:postgres@localhost/postgres
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use harsh::Harsh;
 use serde::Deserialize;
+use sqlx::postgres::PgPoolOptions;
 
 // creat a new harshid
 fn get_id() -> String {
@@ -39,6 +40,22 @@ async fn shorten_url(payload: web::Json<ShortenUrl>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     const PORT: u16 = 8080;
     const HOST: &str = "127.0.0.1";
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost:54321")
+        .await
+        .unwrap();
+
+    println!("success connecting to the db");
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    assert_eq!(row.0, 150);
 
     println!("Starting server at http://{}:{}", HOST, PORT);
 
