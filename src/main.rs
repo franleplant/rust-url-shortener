f03fe7c76555a696a39c63829377ea3ef889cbe9
Fran Guijarro <franleplant@gmail.com>
2022-11-11 12:57:08 -0300

    add actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use harsh::Harsh;
 
 // creat a new harshid
@@ -7,8 +8,31 @@ fn get_id() -> String {
     harsh.encode(&random_vec)
 }
 
-fn main() {
+/* fn main() {
     println!("Hello, world! {}", get_id());
     println!("Hello, world! {}", get_id());
     println!("Hello, world! {}", get_id());
+} */
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    const PORT: u16 = 8080;
+    const HOST: &str = "127.0.0.1";
+
+    println!("Starting server at http://{}:{}", HOST, PORT);
+
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind((HOST, PORT))?
+        .run()
+        .await
 }
